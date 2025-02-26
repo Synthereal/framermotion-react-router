acefe6fdef06dc0e29eb5601bd7cd9ef33261844
Synthereal <infernofire027@gmail.com>
2025-02-26 08:12:41 -0800

    my attempt

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,42 +1,65 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import ProgressiveImage from "react-progressive-image";
+import { motion } from "framer-motion";
 
-const Home = ({ imageDetails, image }) => (
-  <>
-    <main>
-      <div className='container'>
-        <div className='row center'>
-          <div className='image-container'>
-            <div
-              className='thumbnail'
-              ref={image}
-              style={{
-                width: imageDetails.width,
-                height: imageDetails.height,
-              }}>
-              <div className='frame'>
-                <Link to={`/model/yasmeen-tariq`}>
-                  <ProgressiveImage
-                    src={require("../images/yasmeen.webp")}
-                    placeholder={require("../images/compressed-image.jpg")}>
-                    {(src) => <img src={src} alt='Yasmeen Tariq' />}
-                  </ProgressiveImage>
-                </Link>
+// const transition = { duration: 0.6, ease: [0.6, 0.01, -0.05, 0.9] };
+const transition = { duration: 0.6, ease: [0.43, 0.13, 0.23, 0.96] };
+
+const Home = ({ imageDetails, image }) => {
+  const [locked, setLocked] = useState(false);
+
+  return (
+    <>
+      <main>
+        <div className="container">
+          <div className="row center">
+            <div className="image-container">
+              <div
+                className="thumbnail"
+                ref={image}
+                style={{
+                  width: imageDetails.width,
+                  height: imageDetails.height,
+                }}
+              >
+                <div className="frame">
+                  <Link to={`/model/yasmeen-tariq`}>
+                    <ProgressiveImage
+                      src={require("../images/yasmeen.webp")}
+                      placeholder={require("../images/compressed-image.jpg")}
+                    >
+                      {(src) => (
+                        <motion.img
+                          src={src}
+                          whileHover={locked ? undefined : { scale: 1.1 }}
+                          animate={locked ? { scale: 1.1 } : {}}
+                          transition={transition}
+                          onClick={() => setLocked(true)}
+                          alt="Yasmeen Tariq"
+                        />
+                      )}
+                    </ProgressiveImage>
+                  </Link>
+                </div>
               </div>
-            </div>
-            <div className='information'>
-              <div className='title'>Yasmeen Tariq</div>
-              <div className='location'>
-                <span>28.538336</span>
-                <span>-81.379234</span>
-              </div>
+              <motion.div
+                exit={{ opacity: 0 }}
+                transition={transition}
+                className="information"
+              >
+                <div className="title">Yasmeen Tariq</div>
+                <div className="location">
+                  <span>28.538336</span>
+                  <span>-81.379234</span>
+                </div>
+              </motion.div>
             </div>
           </div>
         </div>
-      </div>
-    </main>
-  </>
-);
+      </main>
+    </>
+  );
+};
 
 export default Home;
